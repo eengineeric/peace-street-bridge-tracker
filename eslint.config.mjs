f2668fef2127e9f4374a7b778c08b19006cca985
eengineeric <dblahm@outlook.com>
2026-07-30 23:07:09 -0400

    Add ESLint configuration for Next.js project

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import js from "@eslint/js";
+import nextPlugin from "@next/eslint-plugin-next";
+
+export default [
+  js.configs.recommended,
+  {
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    rules: {},
+  },
+];
